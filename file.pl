beb768a5391fb5b9716995e234f4882c37ae55c6
Yue Kang <ykan215@cse.unsw.EDU.AU>
2014-10-01 12:15:45 +1000

    resolved function error for file.pl. now need to fix another bug for subset1

--- file.pl
+++ file.pl
@@ -11,6 +11,35 @@
 	"elif" => "elsif"
 );
 
+sub convertVar{
+		my $check = $line;
+    if ($check =~ /(\w+) = ([a-z0-9].*)/cig){
+      $variable = $1;
+      $type = $2;
+      if ($type =~ /\w+/){
+        $type = "str";
+      } else {
+        $type = "num";
+      }
+      if (!defined($var{$variable})){
+      	$vartype{$variable}= $type;
+        $var{$variable} = $variable;
+        $check =~ s/$variable/\$$variable/ig;
+      }
+    } else {
+      @words = split(' ',$line);
+      foreach $word (@words){
+        if ($word =~ /(\w+)/cig){
+          $word = $1;
+          if (defined($var{$word})){
+          $check =~ s/$word/\$$word/ig
+          }
+        }
+      }
+    }
+	return $check;
+}
+
 sub convert {
   my @lines = @_;
     
@@ -30,32 +59,45 @@ sub convert {
 	  }
 
 		#check for variables	  
-	  if ($line =~ /(\w+) = ([a-z0-9].*)/cig){
-			if(!defined($functions{$1})){
-				$variable = $1;
-				$type = $2;
-				if (!defined($var{$variable})){
-					$var{$variable} = $variable;
-					$vartype{$variable} = $type;
-				}
-				$line =~ s/$variable/\$$variable/ig;
-			}
-		} else {
-			@words = split(' ', $line);
-			foreach $word (@words){
-				if ($word =~ /(\w+)/cig){
-					$word = $1;
-					if (!defined($functions{$word}) && !($line =~ /print/ig)){
-						$line =~ s/$word/\$$word/ig;
-					}
-				}
-			}
+    if ($line =~ /(\w+) = ([a-z0-9].*)/cig || $line =~ /(\w+)/ig){
+    	$line = &convertVar($line);
 		}
-    print "$line\n";
+
   }
   return @lines;
 }
 
 @lines = &convert(@lines);
+
+
+foreach $line (@lines){
+	print "$line\n";
+}
+=begin comment
+ if ($line =~ /(\w+) = ([a-z0-9].*)/cig){
+      $variable = $1;
+      $type = $2;
+      if ($type =~ /\w+/){
+        $type = "str";
+      } else {
+        $type = "num";
+      }
+      if (!defined($var{$variable})){
+      	$vartype{$variable}= $type;
+        $var{$variable} = $variable;
+        $line =~ s/$variable/\$$variable/ig;
+      }
+    } else {
+      @words = split(' ',$line);
+      foreach $word (@words){
+        if ($word =~ /(\w+)/cig){
+          $word = $1;
+          if (defined($var{$word})){
+          $line =~ s/$word/\$$word/ig
+          }
+        }
+      }
+    }
+=end comment
 
 
