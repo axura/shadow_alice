0f5b8fbd8981698b2e37e845b3fecbe06d087025
Yue Kang <ykan215@cse.unsw.EDU.AU>
2014-10-02 03:34:10 +1000

    updated the file test1.pl with new functions for comparators and if/while statements

--- test1.pl
+++ test1.pl
@@ -10,6 +10,27 @@
 	"elif" => "elsif"
 );
 
+%strcmp = (
+  "<>" => "ne",
+  "<" => "lt",
+  ">" => "gt",
+  "<=" => "le",
+  ">=" => "ge",
+  "==" => "eq",
+  "!=" => "ne"
+);
+
+%numcmp = (
+  "<=>" => "!=",
+  "<" => "<",
+  ">" => ">",
+  "==" => "==",
+  "<=" => "<=",
+  "=>" => "=>",
+  "!=" => "!=",
+	'=' => '='
+);
+
 
 sub convert {
 	my @lines = @_;
@@ -20,11 +41,14 @@ sub convert {
 		while ($line =~ /(\w+)\s*=/ig){
 			$variable = $1;
 			#$type = $2;
-			if ((!defined($var{$variable})) && !(defined($functions{$variable}))){
-				print "$variable\n";
+			if (!(defined($var{$variable})) && !(defined($functions{$variable}))){
 				$var{$variable} = $variable;
+				print "$var{$variable}\n";
 			}
 		}
+
+		#comparator change for numerals:
+		$line =~ s/<=>/!=/ig;
 
 		#changing the variables
 		@words = split(' ', $line);
@@ -33,22 +57,42 @@ sub convert {
 			#my $variable = $1;
 			chomp($variable);
 			if (defined($var{$variable})){
-				print "hello\n";
-				if ($line =~ / $variable /ig){
+				if ($line =~ /\s$variable\s{1}/i){
 					print "1\n";
-					$line =~ s/ $variable / \$$variable /g;
-				} elsif ($line =~ /$variable[\w]{1}/ig){
+					$line =~ s/ $variable / \$$variable /;
+				} elsif ($line =~ /$variable[\w]{1}/i){
 					print "2\n";
-					$line =~ s/ $variable / \$$variable/ig;
-				} elsif ($line =~ /^$variable\s*=/ig){
+					$line =~ s/ $variable / \$$variable/i;
+				} elsif (($line =~ /^$variable\s*=/i) && !($variable =~ /\$/i)){
 					print "3\n";
-					$line =~ s/$variable/\$$variable/ig; 
+					$line =~ s/$variable/\$$variable/i; 
 				} else{
-					$line =~ s/$variable/\$$variable/ig;
+					$line =~ s/$variable/\$$variable/i;
 				}
 			}
 		}
 
+		#differentiating if and while loops
+		if (!($line =~ /:[\s]*$/)){
+			if ($line =~ /^([\w]+)\s*/){
+				$function = $1;
+				if (defined($functions{$function})){
+					print "$functions{$function}\n";
+				}
+			}
+		}
+
+		#check for str and numerical comparators
+		if ($line =~ /(\w+) ([<>=].*?) (\w+)/ig){
+    	$arg1 = $1;
+    	$comp = $2;
+    	$arg2 = $3;
+    	if (($arg1 =~ /\d+/) || ($arg2 =~ /\d+/)){
+    	  $line =~ s/$comp/$numcmp{$comp}/ig;
+    	} else{
+    	  $line =~ s/$comp/$strcmp{$comp}/ig;
+    	}
+  	}
 
 	  if ($line =~ /^#!/){
 		$line =~ s/python/perl -w/ig;
