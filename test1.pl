e32421e8c7df2672c6366a9ae6406b7543cea589
Yue Kang <ykan215@cse.unsw.EDU.AU>
2014-10-02 16:57:59 +1000

    updated the test1.pl partially works for printing multiline if statements

--- test1.pl
+++ test1.pl
@@ -22,7 +22,8 @@
   "<=" => "le",
   ">=" => "ge",
   "==" => "eq",
-  "!=" => "ne"
+  "!=" => "ne",
+	"<=>"	=> "ne",
 );
 
 %numcmp = (
@@ -33,12 +34,15 @@
   "<=" => "<=",
   "=>" => "=>",
   "!=" => "!=",
-	'=' => '='
+	"=" => "="
 );
 
 
 sub convert {
 	my @lines = @_;
+	my $index = 0;
+	my $multiline_statement = 0;
+	my $pre_indlen = 0;
 	foreach $line (@lines){
 	  $changed = 0;
     chomp($line);
@@ -48,7 +52,6 @@ sub convert {
 			#$type = $2;
 			if (!(defined($var{$variable})) && !(defined($functions{$variable}))){
 				$var{$variable} = $variable;
-				print "$var{$variable}\n";
 			}
 		}
 
@@ -59,17 +62,13 @@ sub convert {
 		@words = split(' ', $line);
 		foreach $variable (@words){
 		#while ($line =~ m/\s+([\w+])\s*/ig || $line =~ m/^([\w+])\s*/ig){
-			#my $variable = $1;
 			chomp($variable);
 			if (defined($var{$variable})){
 				if ($line =~ /\s$variable\s{1}/i){
-				#	print "1\n";
 					$line =~ s/ $variable / \$$variable /;
 				} elsif ($line =~ /$variable[\w]{1}/i){
-				#	print "2\n";
 					$line =~ s/ $variable / \$$variable/i;
 				} elsif (($line =~ /^$variable\s*=/i) && !($variable =~ /\$/i)){
-				#	print "3\n";
 					$line =~ s/$variable/\$$variable/i; 
 				} else{
 					$line =~ s/$variable/\$$variable/i;
@@ -87,13 +86,45 @@ sub convert {
 					$line =~ s/$function/$function(/ig;					
         	$line =~ s/:\s/){\n\t/ig;
 					$line =~ s/\s*$/\n}/ig;
-				}
-			}
-		}else {
-        if (defined($loopfunctions{$function})){
+				} elsif (defined($loopfunctions{$function})){
           $line =~ s/$function/$loopfunctions{$function}/ig;
 				}
-     }
+			}
+		}
+
+		#checking for multiline
+		if ($line =~ /^(\s*)/){
+			$curr_indentation = length($1);
+			if ($multiline_statement == 0){
+				$pre_indlen = $curr_indentation;
+				$indentation = $1;
+			} elsif ($curr_indentation > $pre_indlen){
+				$multiline_statement = 1;
+			} elsif ($curr_indentation == $pre_indlen && $multiline_statement == 1){
+				$line =~ s/^/$indentation}\n/i;
+				$multiline_statement = 0;
+			}
+		}
+
+		if ($line =~ /:\s*$/i){
+			if ($line =~ /^([\s]*)(\w+)/i){
+			#print "multiline: $line";
+				$function = $2;
+				$multiline_statement = 1;
+				if (!($line =~ /else/)){ 
+					$line =~ s/$function/$function(/;
+					$line =~ s/:\s*$/ ){/i;
+				} else {
+					$line =~ s/:\s*$/ {/i;
+				}
+			}
+		} 
+	
+
+		#check for imported modules like sys. 
+		if ($line =~ /import sys/i){
+			print "sys module imported\n";			
+		}
 
 		#check for str and numerical comparators
 		if ($line =~ /(\w+) ([<>=].*?) (\w+)/ig){
@@ -102,7 +133,7 @@ sub convert {
     	$arg2 = $3;
     	if (($arg1 =~ /\d+/) || ($arg2 =~ /\d+/)){
     	  $line =~ s/$comp/$numcmp{$comp}/ig;
-    	} else{
+    	} elsif (defined($strcmp{$comp})){
     	  $line =~ s/$comp/$strcmp{$comp}/ig;
     	}
   	}
@@ -112,12 +143,14 @@ sub convert {
 	  } elsif ($line =~ /^\s*print\s*"(.*)"\s*$/){
 		  	#$variable =~ $1;
       	$line =~ s/$/,"\\n";/ig;
-	  } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/ || $line =~ /\s*}\s*/){
+	  } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/ || $line =~ /[}{]/){
 		  $changed = -1;
 	  } else{
 		  $changed = 1;
 		  $line =~ s/$/;/ig;
 	  }
+
+		$index += 1; #increment the index tracker
 	}
 	return @lines;
 }
