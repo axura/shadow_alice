a15c4424683bb590842a5d1e75f90523eee74314
Yue Kang <ykan215@cse.unsw.EDU.AU>
2014-10-03 21:59:14 +1000

    tested variables with test00.py and test01.py

--- final.pl
+++ final.pl
@@ -1,82 +1,65 @@
 #!/usr/bin/perl -w
 #
-@code = <STDIN>;
-$code[0] =~ s/python/perl -w/ig;
+%var = (); #hash for storing variables
 
 %functions = (
-#  "print" => "print",
   "if" => "if",
+  "elif" => "elsif",
   "else" => "else",
   "while" => "while",
-  "break" => "last",
-  "continue" => "next"
 );
 
-sub semicolon{
-  my @lines = @_;
-  foreach $line (@lines){
-    if (!($line =~ /^\s*#/ig)and !($line =~ /^\s*$/ig)){
-      $line =~ s/\n/;\n/gi if($line ne $code[0]);
-    }
-  }
-  return @lines;
-}
+%loopfunctions = (
+  "break" => "last",
+  "continue" => "next",
+  );
 
-sub checkPrint{
-  my @lines = @_;
-  foreach $line (@lines){
-    if ($line =~ /print/cgi){
-      $line =~ s/,/," ",/ig;
-      $line =~ s/\n/,"\\n";/ig;
-    }
-  }
-  return @lines;
-}
+%other = (
+  "print" => "print",
+  "for" => "foreach",
+  "import" => "import",
+  "in" => "in",
+  "range" => "range",
+  "len" => "length"
+);
 
 sub convertVar{
-  my @lines = @_;
-  my %var = ();
-  foreach $line (@lines){
-    if ($line =~ /(\w+) =/cig){
-      $variable = $1;
-      $var{$variable} = $variable;
-      $line =~ s/$variable/\$$variable/ig;
-    } else {
-      @words = split(' ',$line);
-      foreach $word (@words){
-        if ($word =~ /(\w+)/cig){
-          $word = $1;
-          if (defined($var{$word})){
-          $line =~ s/$word/\$$word/ig
-          }
-        }
+	my $check = $line;
+	$check =~ s/:/ :/ig;
+  $check =~ s/[\)\(]/ [\)\(] /ig;	
+	my @words = split(' ',$check);
+  foreach my $word (@words){
+    if (($word =~ /\w+/i) && ($word =~ /^[a-z]/i)){
+      if(!defined($functions{$word}) && !defined($loopfunctions{$word}) && !defined($other{$word})){
+        $var{$word} = $word;
+        $word = '$'.$word;
       }
     }
   }
-  return @lines;
+  $check = join(' ', @words);
+
+	return $check;
 }
 
-#converts default functions such as while or if
-sub defaultFunctions {
-  my @lines = @_;
-  foreach $line (@lines){
-    if ($line =~ /(\w+)/ig){
-      $function = $1;
-      if (defined($functions{$function})){
-        $line =~ s/$function/$function(/ig;
-        $line =~ s/:/){/ig;
-        $line =~ s/\n/}\n/ig;
-      }
-    }  
-  }
-  return @lines;
-}
+while ($line = <>) {
+	chomp ($line);
+	$line = &convertVar($line);
 
-@code = &checkPrint(@code);
-@code = &semicolon(@code); #convert this at the very last
-@code = &convertVar(@code);
-@code = &defaultFunctions(@code);
-
-foreach $line (@code){
-  print "$line"
+	if ($line =~ /^#!/) {	
+		# translate #! line 		
+		print "#!/usr/bin/perl -w\n";
+	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+		# Blank & comment lines can be passed unchanged		
+		print $line;
+	} elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+		# Python's print print a new-line character by default
+		# so we need to add it explicitly to the Perl print statement
+		
+		print "print \"$1\\n\";\n";
+	} else {
+	
+		# Lines we can't translate are turned into comments
+		
+		print "$line;\n";
+	}
 }
