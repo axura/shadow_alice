4644225d3308a41fb9035c5c48eec71f38a86138
Yue Kang <ykan215@cse.unsw.EDU.AU>
2014-10-04 00:22:46 +1000

    added some changes to the function for single line statements. found bug in printing. going to fix

--- final.pl
+++ final.pl
@@ -26,7 +26,12 @@
 sub convertVar{
 	my $check = $line;
 	$check =~ s/:/ :/ig;
-  $check =~ s/[\)\(]/ [\)\(] /ig;	
+  $check =~ s/\)/ \) /ig;
+  $check =~ s/\(/ \( /ig;
+# $check =~ s/[\+\*\/\-]/ [\+\*\/\-] /ig;
+  if ($line =~ /^(\s*)/i){
+    $indentation = $1;
+  }
 	my @words = split(' ',$check);
   foreach my $word (@words){
     if (($word =~ /\w+/i) && ($word =~ /^[a-z]/i)){
@@ -37,21 +42,79 @@ sub convertVar{
     }
   }
   $check = join(' ', @words);
+  $check = $indentation.$check;
 
 	return $check;
 }
 
+sub convertsingle{
+	$check = $line;
+	if ($line =~ /(\w+\s.+:)\s\w+/){
+		$condition = $1;
+		$condition =~ s/if/if \(/ig;
+		$condition =~ s/while/while \(/ig;
+		$condition =~ s/:/){\n/ig;
+	}
+#	$check =~ s/if/if(/i;
+#	$check =~ s/while/while(/i;
+#	$check =~ s/:/){\n/i;
+#	print "singleline: $line\n";
+		$operations = $check;
+		$operations =~ s/if\s.+://ig;
+		$operations =~ s/while\s.+://ig;
+		@lines = split(';', $operations);
+		foreach $action (@lines){
+			#$action =~ s/$/\n/i;
+			$action =~ s/^\s*/$indentation\t/i;
+		}
+		$operations = join("\n", @lines);
+		#print "operation: $operations\n";
+		$operations = $operations."\n".$indentation."\n}";
+		$operations = $condition.$operations;
+
+	return $operations;
+}
+
+
 while ($line = <>) {
 	chomp ($line);
-	$line = &convertVar($line);
 
+	$line = &convertVar($line);
+  $line = $indentation.$line;
+
+	if (($line =~ /if\s.+:\s\w+/) || ($line =~ /while\s.+:\s\w+/)){
+		print "hello\n";
+		$line = &convertsingle($line);
+	}
+
+	if ($line =~ /^#!/) {	
+		# translate #! line 		
+		$line =~ s/python/perl -w/ig;
+	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+		# Blank & comment lines can be passed unchanged		
+		#$change = 0;
+	} elsif ($line =~ /\s*print\s*"(.*)"\s*$/ || $line =~ /print/) {
+		# Python's print print a new-line character by default
+		# so we need to add it explicitly to the Perl print statement
+		$line =~ s/\s*$/,"\\n";/ig;
+	} else {
+		# Lines we can't translate are turned into comments
+		$line =~ s/$/;/ig;
+	}
+
+	print "$line\n";
+
+}
+
+
+=begin comment
 	if ($line =~ /^#!/) {	
 		# translate #! line 		
 		print "#!/usr/bin/perl -w\n";
 	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
 		# Blank & comment lines can be passed unchanged		
 		print $line;
-	} elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+	} elsif ($line =~ /\s*print\s*"(.*)"\s*$/) {
 		# Python's print print a new-line character by default
 		# so we need to add it explicitly to the Perl print statement
 		
@@ -60,6 +123,7 @@ while ($line = <>) {
 	
 		# Lines we can't translate are turned into comments
 		
-		print "$line;\n";
+    print "$line;\n";
 	}
 }
+=end comment
