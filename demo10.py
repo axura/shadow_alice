cf8e9299ab4cac92f45ccddb3690346729814eb5
Yue Kang <ykan215@cse.unsw.EDU.AU>
2014-10-06 04:06:32 +1100

    added demo10.py

--- demo10.py
+++ demo10.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 
+#projec Euler problem 6: square sum of numbers.
+#solution referenced from http://www.mathblog.dk/project-euler-problem-6/  
 
+sumtotal = 0
+squared = 0
+result = 0
+ 
+N = 100
+ 
+sumtotal = N * ( N + 1 ) / 2
+squared = (N * (N + 1) * (2 * N + 1)) / 6
+ 
+result = sumtotal * sumtotal - squared
+
+print result
